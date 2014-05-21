219020c1fb0aaee7325653ba4918fe8630ea8349
Lacc Riophae <iruif.cn@gmail.com>
2014-05-21 16:32:05 +0800

    细节修正

--- js/bg.js
+++ js/bg.js
@@ -188,7 +188,7 @@ function initUrlExpand() {
 	var short_url_services = lscache.get('short_url_services');
 	if (short_url_services) {
 		// 识别更多短链接
-		short_url_services['[a-z0-9]{1,5}\.[a-z]{2,3}'] = true;
+		short_url_services['[a-z0-9]{1,5}\\.[a-z]{2,3}'] = true;
 		var re = '^https?:\\/\\/';
 		re += '(?:' + Object.keys(short_url_services).join('|') + ')';
 		re += '\\/\\S+';
